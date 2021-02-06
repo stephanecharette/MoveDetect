dbd4159953d7011ea04c1e2c38d2dd34180104d2
Stephane Charette <stephane@hummingbirds.ai>
2021-02-06 08:22:58 -0800

    use nanoseconds instead of milliseconds to get more precise timings

--- src-test/main.cpp
+++ src-test/main.cpp
@@ -31,19 +31,24 @@ int main(int argc, char *argv[])
 		const double input_fps			= video_input.get(cv::VideoCaptureProperties::CAP_PROP_FPS			);
 		const double original_width		= video_input.get(cv::VideoCaptureProperties::CAP_PROP_FRAME_WIDTH	);
 		const double original_height	= video_input.get(cv::VideoCaptureProperties::CAP_PROP_FRAME_HEIGHT	);
+		const double total_frames		= video_input.get(cv::VideoCaptureProperties::CAP_PROP_FRAME_COUNT	);
+		const double length_in_seconds	= total_frames / input_fps;
 		const int desired_width			= std::round(zoom_factor * original_width);
 		const int desired_height		= std::round(zoom_factor * original_height);
-		const int frame_length			= std::round(1000.0 / input_fps);
+		const size_t frame_length_ns	= std::round(1000000000.0 / input_fps);
 		const auto desired_frame_size	= cv::Size(desired_width, desired_height);
 
 		std::cout
-			<< ""																		<< std::endl
-			<< "Input video .......... " << argv[video_index]							<< std::endl
-			<< "Zoom factor .......... " << zoom_factor									<< std::endl
-			<< "Original dimensions .. " << original_width	<< "x" << original_height	<< std::endl
-			<< "Desired dimensions ... " << desired_width	<< "x" << desired_height	<< std::endl
-			<< "Frame rate ........... " << input_fps		<< " FPS"					<< std::endl
-			<< "Frame length ......... " << frame_length	<< " milliseconds"			<< std::endl;
+			<< ""																						<< std::endl
+			<< "Input video .......... " << argv[video_index]											<< std::endl
+			<< "Number of frames ..... " << total_frames												<< std::endl
+			<< "Length of video ...... " << length_in_seconds				<< " seconds"				<< std::endl
+			<< "Zoom factor .......... " << zoom_factor													<< std::endl
+			<< "Original dimensions .. " << original_width					<< "x" << original_height	<< std::endl
+			<< "Desired dimensions ... " << desired_width					<< "x" << desired_height	<< std::endl
+			<< "Frame rate ........... " << input_fps						<< " FPS"					<< std::endl
+			<< "Frame interval ....... " << frame_length_ns					<< " nanoseconds"			<< std::endl
+			<< "Frame interval ....... " << (frame_length_ns / 1000000.0)	<< " milliseconds"			<< std::endl;
 
 		MoveDetect::Handler movement_detection;
 		movement_detection.mask_enabled				= true;
@@ -79,8 +84,9 @@ int main(int argc, char *argv[])
 			video_output.open("output_" + std::to_string(std::time(nullptr)) + ".mp4", cv::VideoWriter::fourcc('m', 'p', '4', 'v'), input_fps, mat.size());
 		}
 
-		const std::chrono::high_resolution_clock::duration duration = std::chrono::milliseconds(frame_length);
-		std::chrono::high_resolution_clock::time_point next_frame_time_point = std::chrono::high_resolution_clock::now() + duration;
+		const std::chrono::high_resolution_clock::duration		duration				= std::chrono::nanoseconds(frame_length_ns);
+		const std::chrono::high_resolution_clock::time_point	start_time				= std::chrono::high_resolution_clock::now();
+		std::chrono::high_resolution_clock::time_point			next_frame_time_point	= start_time + duration;
 
 		while (true)
 		{
@@ -135,6 +141,12 @@ int main(int argc, char *argv[])
 				next_frame_time_point = now + duration;
 			}
 		}
+
+		const std::chrono::high_resolution_clock::time_point end_time = std::chrono::high_resolution_clock::now();
+
+		std::cout
+			<< "-> processed " << movement_detection.next_frame_index << " frames" << std::endl
+			<< "-> time elapsed: " << std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count() << " milliseconds" << std::endl;
 	}
 
 	return 0;
